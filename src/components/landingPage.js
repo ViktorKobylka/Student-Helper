34a0fbb328464606a297cf189f56a746a893b378
ViktorKobylka <g00425163@atu.ie>
2025-03-18 19:20:12 +0000

    added logotype to landingPage structure

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -1,7 +1,10 @@
+import logotype from '../styles/images/logotype.jpg';
+
 //LandingPage component: represents the landing page of the "Student Helper" website
 const LandingPage = ()=>{
     return(
         <div className="landingPage-cont">
+            <img src={logotype} alt="logotype" />
             <h1>Welcome to Student Helper</h1>
             <button>Log In</button>
             <button>Register</button>
