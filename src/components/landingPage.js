d460483b8df3ea60ad3b61e3bb0d9415075808c9
ViktorKobylka <g00425163@atu.ie>
2025-03-18 19:12:18 +0000

    added landing page structure and logotype image

---
+++ src/components/landingPage.js
@@ -0,0 +1,12 @@
+//LandingPage component: represents the landing page of the "Student Helper" website
+const LandingPage = ()=>{
+    return(
+        <div className="landingPage-cont">
+            <h1>Welcome to Student Helper</h1>
+            <button>Log In</button>
+            <button>Register</button>
+        </div>
+    );
+}
+
+export default LandingPage;
