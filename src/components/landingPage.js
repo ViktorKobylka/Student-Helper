82930848467c15348995a7987fa9c4790006077f
ViktorKobylka <g00425163@atu.ie>
2025-03-26 17:31:58 +0000

    added reg popup

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -36,7 +36,29 @@ const LandingPage = ()=>{
                             </div>
                         )}
                     </Popup>
-                    <button>Register</button>
+                    <Popup trigger={<div><button>Register</button></div>} modal>
+                        {close => (
+                            <div className="modal">
+                                <button className="close-btn" onClick={() => close()}>X</button>
+                                <h2>Register</h2>
+                                <div>
+                                    <label>Username:</label>
+                                    <input type="text" placeholder="Enter your username" />
+                                </div>
+                                <div>
+                                    <label>Email:</label>
+                                    <input type="email" placeholder="Enter your email" />
+                                </div>
+                                <div>
+                                    <label>Password:</label>
+                                    <input type="password" placeholder="Enter your password" />
+                                </div>
+                                <div>
+                                    <button className="login-btn">Register</button>
+                                </div>
+                            </div>
+                        )}
+                    </Popup>
                 </div>
             </div>
         </div>
